843d0da8e77889831a906efd2679755ad5aa3de4
Flavia Pittet <pittet.flavia@gmail.com>
2016-02-25 11:39:28 +0100

    all controllers have GET and POST methods implemented

--- app/controllers/comments.js
+++ app/controllers/comments.js
@@ -1,18 +1,39 @@
 var express = require('express'),
     router = express.Router(),
     mongoose = require('mongoose'),
-    Article = mongoose.model('Article');
+    Comment = mongoose.model('Comment');
 
 module.exports = function (app) {
-    app.use('/', router);
+    app.use('/api/comments', router);
 };
-
+/**
+ * gets all comments
+ */
 router.get('/', function (req, res, next) {
-    Article.find(function (err, articles) {
-        if (err) return next(err);
-        res.render('index', {
-            title: 'Generator-Express MVC',
-            articles: articles
-        });
+    Comment.find(function (err, comments) {
+        if (err) {
+            res.status(500).send(err);
+            return;
+        }
+
+        res.send(comments);
     });
+});
+/**
+ creates a new comment
+ **/
+
+router.post('/', function (req, res, next) { //chemin relatif a "api/people"
+    /*res.send("Hello World!");*/
+
+    var comment = new Comment(req.body);
+
+    comment.save(function (err, createdComment) {
+        if (err) {
+            res.status(500).send(err); // pas propre car donne infos au client
+            return; //arrête la fonction. ATTENTION - LE METTRE car sinon CRASH du serveur
+        }
+        res.send(createdComment);
+    });
+
 });
