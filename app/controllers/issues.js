a7cf0af9ecdb8adc33f4aa28b8c815f2dc743c69
Flavia Pittet <pittet.flavia@gmail.com>
2016-02-25 22:03:07 +0100

    users get citizens or staff methods created and working

--- app/controllers/issues.js
+++ app/controllers/issues.js
@@ -11,6 +11,83 @@ var express = require('express'),
 module.exports = function (app) {
     app.use('/api/issues', router);
 };
+
+
+/**
+ * MIDDLEWARES
+ */
+
+/**
+ * Middleware that finds the book corresponding to the :id URL parameter
+ * and stores it in `req.book`.
+ */
+function findIssue(req, res, next) {
+
+    var query = Issue
+        .findById(req.params.id);
+
+    if (req.query.embed == 'user') {
+        query = query.populate('user');
+    } //TODO WTF does this do ???
+
+    query.exec(function (err, issue) {
+        if (err) {
+            res.status(500).send(err);
+            return;
+        } else if (!issue) {
+            res.status(404).send('Issue not found');
+            return;
+        }
+
+        // Store the issue in the request.
+        req.issue = issue;
+
+        next();
+    });
+}
+
+/*
+ function findIssuesByUser(req, res, next) {
+ if (!req.body.user) {
+ // If no user ID is given, return an error.
+ res.status(400).send('User ID is required');
+ return;
+ } else if (!mongoose.Types.ObjectId.isValid(req.body.user)) {
+ // If the user ID is not a valid MongoDB ID, no need to execute a query, return an error directly.
+ res.status(400).send('No user with ID ' + req.body.user);
+ return;
+ }
+
+ // Find the user.
+
+ //User.findById(re) //TODO callback function like issue.find but with else if
+
+ /!*var criteria = {
+ user: req.query.user;
+ };*!/
+
+
+ Issue.find(criteria, function (err, issue) {
+ if (err) {
+ res.status(500).send(err);
+ return;
+ } /!*else if (!issue) {
+ // Return an error if the user doesn't exist.
+ res.status(400).send('No user with ID ' + req.body.userId);
+ return;
+ }*!/
+
+ // Store the user in the request.
+ req.issueUser = user;
+
+ // Forward the request to the next middleware.
+ next();
+ });
+ }*/
+/**
+ * ROUTES
+ */
+
 /**
  * creates a new issue
  *
@@ -46,40 +123,22 @@ router.get('/', function (req, res, next) {
 });
 
 
-function findIssuesByUser(req, res, next) {
-    if (!req.body.user) {
-        // If no user ID is given, return an error.
-        res.status(400).send('User ID is required');
-        return;
-    } else if (!mongoose.Types.ObjectId.isValid(req.body.user)) {
-        // If the user ID is not a valid MongoDB ID, no need to execute a query, return an error directly.
-        res.status(400).send('No user with ID ' + req.body.user);
-        return;
-    }
-
-    // Find the user.
-
-    //User.findById(re) //TODO callback function like issue.find but with else if
-
-   /* var criteria = {
-        user: req.query.user;
-    };*/
-
-
-   Issue.find(criteria, function (err, issue) {
+/**
+ * delete an issue
+ */
+router.delete('/:id', findIssue, function (req, res, next) {
+    req.issue.remove(function (err) {
         if (err) {
             res.status(500).send(err);
             return;
-        } /*else if (!issue) {
-            // Return an error if the user doesn't exist.
-            res.status(400).send('No user with ID ' + req.body.userId);
-            return;
-        }*/
+        }
 
-        // Store the user in the request.
-        req.issueUser = user;
-
-        // Forward the request to the next middleware.
-        next();
+        res.sendStatus(204);
     });
-}
+});
+/**
+ * get an issue
+ */
+router.get('/:id', findIssue, function (req, res, next) {
+    res.send(req.issue);
+});
--- app/controllers/users.js
+++ app/controllers/users.js
@@ -11,23 +11,40 @@ var express = require('express'),
 module.exports = function (app) {
     app.use('/api/users', router);
 };
+
 /**
- * gets all users
+ * MIDDLEWARES
  */
-router.get('/', function (req, res, next) {
-    User.find(function (err, users) {
+
+function findUser(req, res, next) {
+
+    var query = User
+        .findById(req.params.id);
+
+   /* if (req.query.embed == 'user') {
+        query = query.populate('user');
+    } //TODO WTF does this do ???*/
+
+    query.exec(function (err, user) {
         if (err) {
             res.status(500).send(err);
             return;
+        } else if (!user) {
+            res.status(404).send('user not found');
+            return;
         }
 
-        res.send(users);
+        // Store the issue in the request.
+        req.user = user;
+
+        next();
     });
-});
-
-
+}
 /**
- * created a new user
+ * ROUTES
+ */
+/**
+ * creates a new user
  * TODO
  */
 router.post('/', function (req, res, next) { //chemin relatif a "api/people"
@@ -50,3 +67,74 @@ router.post('/', function (req, res, next) { //chemin relatif a "api/people"
 ID Flavia: 56ced63b3376c594473ab8c2
 ID Sabine: 56ced66daa89b1aa47ed6a46
  */
+/**
+ * Get all citizens
+ */
+
+router.get('/citizens', function (req, res, next) {
+    /*res.send("Hello World!");*/
+
+     User.find({'citizen' : "true"}, function (err, users) {
+     if (err) {
+     res.status(500).send(err);
+     return;
+     }
+
+     res.send(users);
+     });
+});
+
+/**
+ * Get all staffs
+ */
+
+router.get('/staffs', function (req, res, next) {
+    /*res.send("Hello World!");*/
+
+    User.find({'staff' : "true"}, function (err, users) {
+        if (err) {
+            res.status(500).send(err);
+            return;
+        }
+
+        res.send(users);
+    });
+});
+/**
+ * gets all users
+ */
+router.get('/', function (req, res, next) {
+    User.find(function (err, users) {
+        if (err) {
+            res.status(500).send(err);
+            return;
+        }
+
+        res.send(users);
+    });
+});
+
+/**
+ * gets a specific user based on id
+ **/
+router.get('/:id', findUser, function (req, res, next) {
+    res.send(req.user);
+});
+
+/**
+ * deletes a user
+ */
+router.delete('/:id', findUser, function (req, res, next) {
+    req.user.remove(function (err) {
+        if (err) {
+            res.status(500).send(err);
+            return;
+        }
+
+        res.sendStatus(204);
+    });
+});
+
+
+
+
