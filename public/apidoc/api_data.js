f152b4fcb51854fc86229f0d13ee437d250edb41
U-SABINEROCHAT\Sabine <sabine.rochat@heig-vd.ch>
2016-02-26 08:58:01 +0100

    add

---
+++ public/apidoc/api_data.js
@@ -0,0 +1,48 @@
+define({ "api": [
+  {
+    "type": "post",
+    "url": "/comments",
+    "title": "Create a comment",
+    "name": "CreateComment",
+    "group": "Comments",
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "description",
+            "description": "<p>Description of the comment.</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "app/controllers/comments.js",
+    "groupTitle": "Comments"
+  },
+  {
+    "type": "post",
+    "url": "/issues",
+    "title": "Create an issue",
+    "name": "CreateIssue",
+    "group": "Issues",
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "type",
+            "description": "<p>Type of the issue.</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "app/controllers/issues.js",
+    "groupTitle": "Issues"
+  }
+] });
---
+++ public/apidoc/api_project.js
@@ -0,0 +1,13 @@
+define({
+  "name": "teaching-heigvd-cm-webs-2016-project",
+  "version": "0.0.1",
+  "description": "",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-02-26T07:36:29.233Z",
+    "url": "http://apidocjs.com",
+    "version": "0.15.1"
+  }
+});
