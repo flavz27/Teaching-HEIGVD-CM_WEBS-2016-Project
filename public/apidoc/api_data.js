44ed43152c47e420c4a7fb07d8185fb09f957555
U-SABINEROCHAT\Sabine <sabine.rochat@heig-vd.ch>
2016-03-06 14:10:26 +0100

    comment issue

--- public/apidoc/api_data.js
+++ public/apidoc/api_data.js
@@ -1116,6 +1116,390 @@ define({ "api": [
     "groupTitle": "Issues"
   },
   {
+    "type": "post",
+    "url": "issues/:id_issue/comments",
+    "title": "Create a comment",
+    "version": "0.0.0",
+    "name": "PostIssueComment",
+    "group": "Issues",
+    "description": "<p>it's function can create a comment for an issue</p>",
+    "examples": [
+      {
+        "title": "Example usage:",
+        "content": "http://localhost/api/issues/4711/comments",
+        "type": "json"
+      }
+    ],
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "description",
+            "description": "<p>The Description of the comments.</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "description",
+            "description": "<p>Description of the issue.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "type",
+            "description": "<p>Type of the issue (according to a list).</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Schema.Types.ObjectId",
+            "optional": false,
+            "field": "user",
+            "description": "<p>User how are created the issue</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "date_created",
+            "description": "<p>Creation date of issue</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Object",
+            "optional": false,
+            "field": "coordonate",
+            "description": "<p>Coordinated the problem (longitude and latitude)</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "coordonate.lat",
+            "description": "<p>Latitude coordinate</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "coordonate.long",
+            "description": "<p>Longitude coordinate</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "status",
+            "description": "<p>Status of the issue</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "[]",
+            "optional": false,
+            "field": "comments_user",
+            "description": "<p>Board with all comments of the issue</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Schema.Types.ObjectId",
+            "optional": false,
+            "field": "comments_user.comment",
+            "description": "<p>A comment of the issue</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "[]",
+            "optional": false,
+            "field": "action",
+            "description": "<p>Board with all actions of the issue</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "action.date",
+            "description": "<p>Creation date of action</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "action.action",
+            "description": "<p>Definition of the action</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Schema.Types.ObjectId",
+            "optional": false,
+            "field": "action.comment",
+            "description": "<p>A comment of the action</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Boolean",
+            "optional": false,
+            "field": "action.current",
+            "description": "<p>If the action is the current = true</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "[]",
+            "optional": false,
+            "field": "tags",
+            "description": "<p>Board with all tags of the issue</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "tags.name",
+            "description": "<p>Name of the tag</p>"
+          }
+        ]
+      }
+    },
+    "error": {
+      "fields": {
+        "Error 4xx": [
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "UnexpectedToken",
+            "description": "<p>The issue has some parameters with uncorrect type</p>"
+          },
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "ValidationError",
+            "description": "<p>There are missing parameters</p>"
+          },
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "Error404",
+            "description": "<p>The server has an unexpected error</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Response (Unexpected Token):",
+          "content": "  <h1>Unexpected token j</h1>\n<h2>400</h2>\n<pre>SyntaxError: Unexpected token j",
+          "type": "json"
+        },
+        {
+          "title": "Response (Validation Error):",
+          "content": "{\"message\": \"Issue validation failed\",\n  \"name\": \"ValidationError\",\n  \"errors\": {\n    \"description\": {\n      \"properties\": {\n        \"type\": \"required\",\n        \"message\": \"Path `{PATH}` is required.\",\n        \"path\": \"description\"\n      },\n      \"message\": \"Path `description` is required.\",\n      \"name\": \"ValidatorError\",\n      \"kind\": \"required\",\n      \"path\": \"description\"\n    }\n  }\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "filename": "app/controllers/issues.js",
+    "groupTitle": "Issues"
+  },
+  {
+    "type": "put",
+    "url": "/issues/:id",
+    "title": "Put an issue with the id",
+    "version": "0.0.0",
+    "name": "PutIssue",
+    "group": "Issues",
+    "description": "<p>it's function can modify an issue with his id</p>",
+    "examples": [
+      {
+        "title": "Example usage:",
+        "content": "http://localhost/api/issues/4711",
+        "type": "json"
+      }
+    ],
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "description",
+            "description": "<p>The Description of the comments.</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "description",
+            "description": "<p>Description of the issue.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "type",
+            "description": "<p>Type of the issue (according to a list).</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Schema.Types.ObjectId",
+            "optional": false,
+            "field": "user",
+            "description": "<p>User how are created the issue</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "date_created",
+            "description": "<p>Creation date of issue</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Object",
+            "optional": false,
+            "field": "coordonate",
+            "description": "<p>Coordinated the problem (longitude and latitude)</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "coordonate.lat",
+            "description": "<p>Latitude coordinate</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "coordonate.long",
+            "description": "<p>Longitude coordinate</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "status",
+            "description": "<p>Status of the issue</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "[]",
+            "optional": false,
+            "field": "comments_user",
+            "description": "<p>Board with all comments of the issue</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Schema.Types.ObjectId",
+            "optional": false,
+            "field": "comments_user.comment",
+            "description": "<p>A comment of the issue</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "[]",
+            "optional": false,
+            "field": "action",
+            "description": "<p>Board with all actions of the issue</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "action.date",
+            "description": "<p>Creation date of action</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "action.action",
+            "description": "<p>Definition of the action</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Schema.Types.ObjectId",
+            "optional": false,
+            "field": "action.comment",
+            "description": "<p>A comment of the action</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Boolean",
+            "optional": false,
+            "field": "action.current",
+            "description": "<p>If the action is the current = true</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "[]",
+            "optional": false,
+            "field": "tags",
+            "description": "<p>Board with all tags of the issue</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "tags.name",
+            "description": "<p>Name of the tag</p>"
+          }
+        ]
+      }
+    },
+    "error": {
+      "fields": {
+        "Error 4xx": [
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "UnexpectedToken",
+            "description": "<p>The issue has some parameters with uncorrect type</p>"
+          },
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "ValidationError",
+            "description": "<p>There are missing parameters</p>"
+          },
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "Error404",
+            "description": "<p>The server has an unexpected error</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Response (Unexpected Token):",
+          "content": "  <h1>Unexpected token j</h1>\n<h2>400</h2>\n<pre>SyntaxError: Unexpected token j",
+          "type": "json"
+        },
+        {
+          "title": "Response (Validation Error):",
+          "content": "{\"message\": \"Issue validation failed\",\n  \"name\": \"ValidationError\",\n  \"errors\": {\n    \"description\": {\n      \"properties\": {\n        \"type\": \"required\",\n        \"message\": \"Path `{PATH}` is required.\",\n        \"path\": \"description\"\n      },\n      \"message\": \"Path `description` is required.\",\n      \"name\": \"ValidatorError\",\n      \"kind\": \"required\",\n      \"path\": \"description\"\n    }\n  }\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "filename": "app/controllers/issues.js",
+    "groupTitle": "Issues"
+  },
+  {
     "type": "delete",
     "url": "/users/:id",
     "title": "Delete one user",
